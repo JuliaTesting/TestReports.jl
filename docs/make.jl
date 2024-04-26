649dd19a7b801aaacd00bceec3ae9122b7473b29
Curtis Vogt <curtis.vogt@gmail.com>
2024-04-26 09:44:27 -0500

    Use `checkdocs=:exports`

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,7 @@ makedocs(
     modules = [TestReports],
     authors =  "Lyndon White, Malcolm Miller and contributors",
     sitename="TestReports.jl",
+    checkdocs=:exports,
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://juliatesting.github.io/TestReports.jl/stable",
